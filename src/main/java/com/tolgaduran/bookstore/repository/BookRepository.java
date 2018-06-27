b020302499859c5fdf8255f005b3605388b604d2
Tolga DURAN (WilLBe) <tolgaduran1980@gmail.com>
2018-06-27 18:23:31 +0300

    Test Dependencies Added to pom.xml

--- src/main/java/com/tolgaduran/bookstore/repository/BookRepository.java
+++ src/main/java/com/tolgaduran/bookstore/repository/BookRepository.java
@@ -5,8 +5,12 @@ import com.tolgaduran.bookstore.model.Book;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
 import java.util.List;
 
+import static javax.transaction.Transactional.TxType.*;
+
+@Transactional(SUPPORTS)
 public class BookRepository {
     @PersistenceContext(unitName = "bookStorePU")
     private EntityManager em;
@@ -15,17 +19,24 @@ public class BookRepository {
         return em.find(Book.class, id);
     }
 
+    @Transactional(REQUIRED)
     public Book create(Book book) {
         em.persist(book);
         return book;
     }
 
+    @Transactional(REQUIRED)
     public void delete(Long id) {
         em.remove(em.getReference(Book.class, id));
     }
 
-    public List<Book> findAll(){
-        TypedQuery<Book> query=em.createQuery("select b from Book b order by b.title",Book.class);
+    public List<Book> findAll() {
+        TypedQuery<Book> query = em.createQuery("select b from Book b order by b.title", Book.class);
         return query.getResultList();
+    }
+
+    public Long countAll(){
+        TypedQuery<Long> query=em.createQuery("select count(b) from Book b",Long.class);
+        return query.getSingleResult();
     }
 }
